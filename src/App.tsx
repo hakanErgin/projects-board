9b0dffe1c520cfa668b15cbef266d1d4dc4461d4
hakanErgin <hakan.ergin@gmail.com>
2020-11-07 03:24:03 +0100

    before installing icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
 import React, { FC } from 'react';
-import { Button } from 'antd';
+import { List, Avatar } from 'antd';
 import './App.css';
+
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
 
 const App: FC = () => (
   <div className="App">
-    <Button type="primary">Button</Button>
+    <List
+      className="ProjectList"
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={(item) => (
+        <List.Item
+          actions={[
+            <a key="list-loadmore-edit">edit</a>,
+            <a key="list-loadmore-more">more</a>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title={<a href="https://ant.design">{item.title}</a>}
+            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
   </div>
 );
 
