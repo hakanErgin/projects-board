a8868bfd0ed337de8c5c03b8ccc80f9d302d2952
hakanErgin <hakan.ergin@gmail.com>
2020-11-08 13:36:46 +0100

    apollo implementation

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import {
   DeleteOutlined,
 } from '@ant-design/icons';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
 
-const data = [
+const ant = [
   {
     title: 'Ant Design Title 1',
   },
@@ -22,36 +23,52 @@ const data = [
   },
 ];
 
-const App: FC = () => (
-  <div className="App">
-    <List
-      header={[
-        <div>
-          Header <div>Header</div>
-        </div>,
-      ]}
-      className="ProjectList"
-      itemLayout="horizontal"
-      dataSource={data}
-      renderItem={(item) => (
-        <List.Item
-          actions={[
-            <UserSwitchOutlined />,
-            <EditOutlined />,
-            <DeleteOutlined />,
-          ]}
-        >
-          <List.Item.Meta
-            avatar={
-              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-            }
-            title={<a href="https://ant.design">{item.title}</a>}
-            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-          />
-        </List.Item>
-      )}
-    />
-  </div>
-);
+const projects = gql`
+  query {
+    allProjects {
+      id
+    }
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(projects);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  if (data) console.log(data);
+
+  return (
+    <div className="App">
+      <List
+        header={[
+          <div>
+            Header <div>Header</div>
+          </div>,
+        ]}
+        className="ProjectList"
+        itemLayout="horizontal"
+        dataSource={ant}
+        renderItem={(item: any) => (
+          <List.Item
+            actions={[
+              <UserSwitchOutlined />,
+              <EditOutlined />,
+              <DeleteOutlined />,
+            ]}
+          >
+            <List.Item.Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+            />
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+}
 
 export default App;
