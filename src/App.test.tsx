0c692365ebb5cc729c1019bf5d02df39239ee906
hakanErgin <hakan.ergin@gmail.com>
2020-11-14 01:53:01 +0100

    init commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import { MockedProvider } from '@apollo/client/testing';
+import { GET_PROJECTS } from './gql/';
+
+it('renders correctly', async () => {
+  const projectsMock = {
+    request: {
+      query: GET_PROJECTS,
+    },
+    result: {
+      data: {
+        allProjects: [
+          {
+            id: '1',
+            name: 'Sub-Ex',
+            Users: [
+              {
+                id: '1',
+              },
+            ],
+          },
+          {
+            id: '2',
+            name: 'Second',
+            Users: [
+              {
+                id: '1',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  };
+
+  const component = renderer.create(
+    <MockedProvider mocks={[projectsMock]} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
+  await new Promise((resolve) => setTimeout(resolve, 0)); // wait for response
+  expect(component).toMatchSnapshot();
 });
